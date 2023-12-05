78b87039d8b50aeabb8c223720a30372cfa752c6
Kotaiba Aal <kotaiba.aal@gmail.com>
2023-12-05 18:27:10 +0100

    Läxa

--- Hindiapp/ContentView.swift
+++ Hindiapp/ContentView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var itemList: [String] = []
+    @State var inputText: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("Enter text", text: $inputText)
+                    .border(Color.gray)
+
+                Button("Lägg till") {
+                    itemList.append(inputText)
+                    inputText = ""
+                }
+            }
+            .padding()
+
+            List(itemList, id: \.self) { item in
+                Text(item)
+            }
+
+            Button("Nollställ") {
+                itemList.removeAll()
+            }
+            .padding()
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
