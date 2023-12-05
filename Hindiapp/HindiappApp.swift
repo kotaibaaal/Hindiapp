6de5d30b37918eac1a9ce39ea59dbe6c6d41cde7
Kotaiba Aal <kotaiba.aal@gmail.com>
2023-12-05 18:17:22 +0100

    Initial Commit

---
+++ Hindiapp/HindiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HindiappApp.swift
+//  Hindiapp
+//
+//  Created by Kotaiba Aal on 2023-12-05.
+//
+
+import SwiftUI
+
+@main
+struct HindiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
